beacdd475a4e616974a72ec5aa5bdefc58153de3
AngryDust <95689293+AngryDust@users.noreply.github.com>
2021-12-07 19:39:59 +0800

    Add files via upload
    Add Files

---
+++ brushflow-Netflix.sh
@@ -0,0 +1,17 @@
+if [ ! -f "fast" ]; then
+     wget https://github.com/ddo/fast/releases/download/v0.0.4/fast_linux_amd64 -O fast
+     chmod +x fast
+fi
+echo "while [ 1 ]" >> _ins.sh
+echo "do" >> _ins.sh
+echo "./fast$1 -d" >> _ins.sh
+echo "sleep 2" >> _ins.sh
+echo "done" >> _ins.sh
+for i in `seq 1 4`
+do
+     cp fast fast$i
+     chmod a+x fast$i
+     nohup bash _ins.sh $i > /dev/null 2>&1 &
+     sleep 1
+     echo "thread $i start!"
+done
