52b873de39c851d267d87baf8db33404b55e3fab
AngryDust <95689293+AngryDust@users.noreply.github.com>
2021-12-14 12:16:45 +0800

    add install speedtest

--- brushflow-Ookla.sh
+++ brushflow-Ookla.sh
@@ -1,3 +1,6 @@
+sudo apt-get install curl
+curl -s https://install.speedtest.net/app/cli/install.deb.sh | sudo bash
+sudo apt-get install speedtest
 echo "while [ 1 ]" > _ins.sh
 echo "do" >> _ins.sh
 echo "speedtest" >> _ins.sh
